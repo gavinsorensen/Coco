9d164db4477c181bb9679813386d22dabaa699ff
gavinsorensen <133609972+gavinsorensen@users.noreply.github.com>
2023-06-16 08:54:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,31 @@
-# server.py
 from flask import Flask, request, jsonify
-from your_ai_module import your_ai_function  # replace with your actual module and function
+import requests
 
 app = Flask(__name__)
 
-@app.route("/api/v1/predict", methods=["POST"])
+@app.route("http://3.237.78.240/api/v1/predict", methods=["POST"])
 def predict():
     data = request.json
     api_key = data.get('api_key')
     user_input = data.get('user_input')
 
-    # verify the api_key and perform the necessary operations.
+    if api_key and user_input:
+        url = "http://3.237.78.240/api/v1/predict"  # Replace with your deployed API URL
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "api_key": api_key,
+            "user_input": user_input
+        }
+        response = requests.post(url, json=payload, headers=headers)
+        
+        if response.status_code == 200:
+            result = response.json().get("result")
+            return jsonify({"result": result})
+        else:
+            error_message = "An error occurred while processing the request."
+            return jsonify({"error": error_message})
 
-    result = your_ai_function(user_input)  # replace with your actual AI function call
-    return jsonify({"result": result})
+    return jsonify({"error": "Invalid API key or user input"})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
