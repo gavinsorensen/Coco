0f7bb4ca29f274b90415eba5ad3b25b18e4064fb
gavinsorensen <133609972+gavinsorensen@users.noreply.github.com>
2023-06-19 15:27:47 -0700

    Update server.py

--- server.py
+++ server.py
@@ -28,4 +28,4 @@ def predict():
     return jsonify({"error": "Invalid API key or user input"})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
